6640af193f4a923f42bbb9a8800e5d92514ce1d3
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-18 18:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue) {
   if (someValue = 199) {
   return "This one is on me!";
 }
- else if (someValue >= 2001) {
+ else if (someValue >= 2000) {
    return "I will gladly take your thirty bucks.";
  }
 }
