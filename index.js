c896cd994a61a9f54238a9cb599b8e005cd64757
mathematical-application-3607 <hannamarie.mehmet@wework.com>
2019-03-18 18:59:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(someValue) {
-  if (someValue = 199) {
+  if (someValue <= 400) {
   return "This one is on me!";
 }
   if (someValue > 2000) {
